98189b56c94a263a52f07207a981757db1f76bce
Samuel <168583510+Slxz00@users.noreply.github.com>
2026-06-17 11:41:27 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+let capitais = ["Curitiba", "São Paulo","Patos de Minas"]
+console.log(capitais[2])
+capitais.push("Manaus")
+console.log(capitais)
+capital.unshft("Vitoria")
+console.log(capitais)
+console.log(capitais[3]);
+capitais.shift();
+console.log(capitais);
+capitais.pop();
+console.log(capitais);
---
+++ splice.js
@@ -0,0 +1,3 @@
+let cores = ("Vermelho", "Azul", "Amarelo")
+cores.splice(1,1,"Vermelho")
+console.log(cores)
